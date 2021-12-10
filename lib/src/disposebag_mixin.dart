28223b1d82b3d2318a9b01cd952a03fe6c274e58
Petrus Nguyễn Thái Học <hoc081098@gmail.com>
2021-12-10 16:01:24 +0700

    remove meta import

--- lib/src/disposebag_mixin.dart
+++ lib/src/disposebag_mixin.dart
@@ -2,7 +2,6 @@ import 'dart:async';
 
 import 'package:disposebag/disposebag.dart';
 import 'package:flutter/widgets.dart';
-import 'package:meta/meta.dart';
 
 /// A mixin that provides the [DisposeBag] that helps disposing Streams and closing Sinks.
 @optionalTypeArgs
